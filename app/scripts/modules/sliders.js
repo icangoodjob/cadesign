cdd75823699bd417caf646f96256560fd562b4b0
Alexey <lxmlshv97@mail.ru>
2023-05-08 18:01:02 +0300

    fix sliders

--- app/scripts/modules/sliders.js
+++ app/scripts/modules/sliders.js
@@ -1,32 +1,65 @@
-import Swiper, { Navigation, Pagination, EffectFade, EffectCreative } from 'swiper';
+import Swiper, { Navigation, Pagination, EffectFade } from 'swiper';
+
+var employeeSwiper = {};
+var sliderType = window.innerWidth <= 991 ? 'mobile' : 'desktop';
 
 const employeeSliderContent = document.querySelector('.employees__slider-content');
-const historySlider = document.querySelector('.history__slider');
-const employeeSwiper = new Swiper(employeeSliderContent, {
-	// configure Swiper to use modules
-	modules: [EffectFade, Navigation, Pagination],
-	wrapperClass: 'employees__slider-wrapper',
-	slideClass: 'employees__slider-item',
-	loop: false,
-	effect: 'fade',
-	fadeEffect: {
-		crossFade: true
-	},
-	centeredSlides: true,
-	navigation: {
-		nextEl: ".slider-arrow-next",
-		prevEl: ".slider-arrow-prev",
-	},
-	pagination: {
-		el: ".slider-pagination",
-		type: 'bullets',
-		clickable: true,
-	},
+
+function initSlider(type) {
+	var sliderSettings = {}
+	if (type === 'mobile') {
+		sliderSettings = {
+			// описание настроек для мобильной вариации.
+			effect: 'slide',
+		}
+	} else {
+		sliderSettings = {
+			// описание настроек для десктопной вариации.
+			effect: 'fade',
+			// allowTouchMove: false,
+			fadeEffect: {
+				crossFade: true
+			},
+		}
+	}
+	// Проверяем, есть ли в объекте слайдера метод destroy, и если есть - вызываем его.
+	if (employeeSwiper.destroy && typeof employeeSwiper.destroy === 'function') {
+		employeeSwiper.destroy(); // Возможно,если в круглые скобки передать true то будет лучше.
+	}
+	employeeSwiper = new Swiper(employeeSliderContent, {
+		// configure Swiper to use modules
+		...sliderSettings,
+		modules: [EffectFade, Navigation, Pagination],
+		wrapperClass: 'employees__slider-wrapper',
+		slideClass: 'employees__slider-item',
+		loop: false,
+		centeredSlides: true,
+		navigation: {
+			nextEl: ".slider-arrow-next",
+			prevEl: ".slider-arrow-prev",
+		},
+		pagination: {
+			el: ".slider-pagination",
+			type: 'bullets',
+			clickable: true,
+		},
+	})
+}
+initSlider(sliderType)
+window.addEventListener("resize", () => {
+	if (window.innerWidth < 991.98 && sliderType == 'desktop') {
+		sliderType = 'mobile'
+		initSlider(sliderType)
+	} else if (window.innerWidth > 991.98 && sliderType == 'mobile') {
+		sliderType = 'desktop'
+		initSlider(sliderType)
+	}
 });
 
+const historySlider = document.querySelector('.history__slider');
 const historySwiper = new Swiper(historySlider, {
 	// configure Swiper to use modules
-	modules: [EffectFade, EffectCreative, Navigation, Pagination],
+	modules: [EffectFade, Navigation, Pagination],
 	wrapperClass: 'history__wrapper',
 	slideClass: 'history__slide',
 	effect: 'fade',
@@ -45,12 +78,12 @@ const historySwiper = new Swiper(historySlider, {
 	},
 });
 const advantagesSlider = document.querySelector('.advantages__wrapper');
-const ourValuessSlider = document.querySelector('.our-values__wrapper');
-let mobileSwiper;
-let mobileSwiperOurValues;
-function mobSwiper() {
-	if (window.innerWidth <= 991.98 && advantagesSlider.dataset.mobile == 'false') {
-		mobileSwiper = new Swiper(advantagesSlider, {
+const ourValuesSlider = document.querySelector('.our-values__wrapper');
+let ourValuesSwiper = null;
+let advantagesSwiper = null;
+function sliderInit() {
+	if (!advantagesSwiper) {
+		advantagesSwiper = new Swiper(advantagesSlider, {
 			modules: [Pagination],
 			slidesPerView: 2,
 			slideClass: 'advantages__item',
@@ -58,7 +91,6 @@ function mobSwiper() {
 			slidesPerGroup: 1,
 			loop: false,
 			spaceBetween: 0,
-			initialSlide: 0,
 			watchSlidesProgress: true,
 			pagination: {
 				el: ".slider-pagination",
@@ -66,12 +98,11 @@ function mobSwiper() {
 				clickable: true,
 			},
 			breakpoints: {
-				320: {
-					watchSlidesProgress: true,
-				},
 			},
 		});
-		mobileSwiperOurValues = new Swiper(ourValuessSlider, {
+	}
+	if (!ourValuesSwiper) {
+		ourValuesSwiper = new Swiper(ourValuesSlider, {
 			modules: [Pagination],
 			slideClass: 'our-values__item',
 			wrapperClass: 'our-values__body',
@@ -93,21 +124,65 @@ function mobSwiper() {
 				},
 			},
 		});
-		advantagesSlider.dataset.mobile = 'true';
-		ourValuessSlider.dataset.mobile = 'true';
-	};
-	if (window.innerWidth > 991) {
-		advantagesSlider.dataset.mobile = 'false';
-		ourValuessSlider.dataset.mobile = 'false';
-		if (advantagesSlider.classList.contains('swiper-container-initialized')) {
-			mobileSwiper.destroy();
-		}
-		if (ourValuessSlider.classList.contains('swiper-container-initialized')) {
-			mobileSwiperOurValues.destroy();
-		}
 	}
-};
-mobSwiper();
-window.addEventListener('resize', () => {
-	mobSwiper();
-});
+}
+function sliderDestroy() {
+	if (advantagesSwiper) {
+		advantagesSwiper.destroy(); // Возможно,если в круглые скобки передать true то будет лучше.
+		advantagesSwiper = null;
+	}
+	if (ourValuesSwiper) {
+		ourValuesSwiper.destroy(); // Возможно,если в круглые скобки передать true то будет лучше.
+		ourValuesSwiper = null;
+	}
+}
+function stateWindow() {
+	// Если ширина экрана меньше или равна mediaQuerySize(991.98)
+	if (window.innerWidth < 991.98) {
+		// Инициализировать слайдер если он ещё не был инициализирован
+		sliderInit()
+	} else if (window.innerWidth > 991.98) {
+		// Уничтожить слайдер если он был инициализирован
+		sliderDestroy()
+	}
+}
+window.addEventListener('resize', stateWindow);
+window.addEventListener('load', stateWindow);
+
+// function mobSwiper() {
+// 	if (window.innerWidth <= 991.98 && advantagesSlider.dataset.mobile == 'false') {
+// 		mobileSwiperOurValues = new Swiper(ourValuesSlider, {
+// 			modules: [Pagination],
+// 			slideClass: 'our-values__item',
+// 			wrapperClass: 'our-values__body',
+// 			slidesPerGroup: 1,
+// 			loop: false,
+// 			spaceBetween: 40,
+// 			initialSlide: 0,
+// 			pagination: {
+// 				el: ".slider-pagination",
+// 				type: 'bullets',
+// 				clickable: true,
+// 			},
+// 			breakpoints: {
+// 				320: {
+// 					slidesPerView: 1,
+// 				},
+// 				767.98: {
+// 					slidesPerView: 2,
+// 				},
+// 			},
+// 		});
+// 		ourValuesSlider.dataset.mobile = 'true';
+// 	};
+// 	if (window.innerWidth > 991.98) {
+// 		ourValuesSlider.dataset.mobile = 'false';
+// 		if (ourValuesSlider.classList.contains('swiper-container-initialized')) {
+// 			mobileSwiperOurValues.destroy();
+// 		}
+// 	}
+// };
+// mobSwiper();
+// window.addEventListener('resize', () => {
+// 	mobSwiper();
+// });
